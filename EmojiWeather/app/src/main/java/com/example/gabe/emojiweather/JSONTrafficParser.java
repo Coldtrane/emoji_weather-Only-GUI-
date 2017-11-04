406fffba27c8e7eba7f9b9b44209393f993becaf
sramocki <sramocki@oakland.edu>
2017-11-04 17:00:10 -0400

    Add files via upload

---
+++ EmojiWeather/app/src/main/java/com/example/gabe/emojiweather/JSONTrafficParser.java
@@ -0,0 +1,36 @@
+package com.example.gabe.emojiweather;
+
+import android.widget.TextView;
+import java.util.regex.Pattern;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+public class JSONTrafficParser {
+
+    //need to accept json object here
+    public String getTraffic(String data){
+
+        try{
+
+            String output = "";
+
+            //Parsing for Weather description
+            String[] parts = data.split("shortDesc\":\"");
+            String[] reports = new String[parts.length];
+            for (int i = 1; i < parts.length; i++)
+            {
+                String[] temp = parts[i].split("\",\"fullDesc\"");
+                output = output + temp[0] +"...";
+            }
+
+            return output;
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+
+            return "No Traffic Incidents Found.";
+        }
+    }
+}
